a938679ddf2758f3151bd10046acc018f6bca846
AASHIQ007 <114137103+AASHIQ007@users.noreply.github.com>
2024-05-21 21:18:56 +0530

    Add files via upload

---
+++ src/Components/Todo.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useRef, useState } from 'react';
+import './CSS/Todo.css'
+import Todoitems from './Todoitems';
+let count =0;
+const Todo = () => {
+
+    const [todos,setTodos]=useState([])
+    const inputRef=useRef(null);
+
+    const add=()=>{
+        setTodos([...todos,{no:count++,text:inputRef.current.value,display:""}])
+        inputRef.current.value="";
+        localStorage.setItem("todos_count",count)
+    }
+    useEffect(()=>{
+        setTodos(JSON.parse(localStorage.getItem("todos")));
+        count = localStorage.getItem("todos_count");
+    },[])
+
+    useEffect(()=>{
+        setTimeout(()=>{
+          console.log(todos);
+          localStorage.setItem( "todos",JSON.stringify(todos))
+        },100);
+    },[todos])
+
+  return (
+    <div className='todo'>
+        <div className='todo-header'>TO-DO LIST</div>
+      <div className='todo-add'>
+        <input ref={inputRef} type="text" placeholder='Add your Task' className='todo-input'/>
+        <div onClick={()=>{add()}} className='todo-add-btn'>ADD</div>
+      </div>
+      <div className='todo-list'>
+        {todos.map((item,index)=>{
+            return <Todoitems key={index} setTodos={setTodos} no={item.no} display={item.display} text={item.text}/>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Todo
---
+++ src/Components/Todoitems.jsx
@@ -0,0 +1,41 @@
+import './CSS/Todoitems.css'
+import tick from './Assets/tick.png'
+import not_tick from './Assets/not_tick.png'
+import cross from './Assets/cross.png'
+const Todoitems = ({no,display,text,setTodos}) => {
+
+  const deleteTodo = (no)=>{
+    let data =JSON.parse(localStorage.getItem("todos"));
+    data=data.filter((todo)=> todo.no!==no);
+    setTodos(data);
+  }
+
+  const toggle=()=>{
+    let data =JSON.parse(localStorage.getItem("todos"));
+    for(let i=0;i<data.length;i++)
+    {
+      if(data[i].no===no){
+        if(data[i].display===""){
+          data[i].display="line-through";
+        }
+        else{
+          data[i].display="";
+        }
+        break;
+      }
+    }
+    setTodos(data);
+  }
+  return (
+    <div className='todoitems'>
+      <div className={`todoitems-container ${display}`} onClick={()=>{toggle(no)}}>
+        {display===""?<img src={not_tick} alt="" />:<img src={tick} alt="" />}
+        
+        <div className="todoitems-text">{text}</div>
+      </div>
+      <img className='todoitems-cross-icon' onClick={()=>{deleteTodo(no)}} src={cross} alt=""/>
+    </div>
+  )
+}
+
+export default Todoitems
